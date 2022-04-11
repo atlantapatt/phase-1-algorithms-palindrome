db3b056d3fee551d215bea42bc79bbc1e04a3f81
AtlantaPatterson <atlantarpatterson@gmail.com>
2022-04-11 16:45:11 -0700

    commit tree 631617fa4edb83073d45e0e0ad914276581afb1e

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  let forwardsStrg = word
+  let reversedStrg = forwardsStrg.split('').reverse().join('')
+  return forwardsStrg === reversedStrg
 }
 
 /* 
